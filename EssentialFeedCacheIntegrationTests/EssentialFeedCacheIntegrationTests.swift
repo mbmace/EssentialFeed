8267e8a00213785d2d86c94d75864fe2d907c13f
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-05-12 14:52:10 +0200

    Include memory leak tracking helper in the cache integration tests target

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,8 +6,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
-
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 }
