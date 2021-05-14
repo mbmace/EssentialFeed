3df803d595ff3fdaf62f3ff4b4ac35d5d2ec0595
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-05-14 13:54:32 +0200

    Render prototype feed data on the table view

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -12,4 +12,14 @@ final class FeedImageCell: UITableViewCell {
     @IBOutlet weak var locationContainer: UIStackView!
     @IBOutlet weak var feedImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(with model: FeedImageViewModel) {
+        locationLabel.text = model.location
+        locationContainer.isHidden = model.location == nil
+        
+        descriptionLabel.text = model.description
+        descriptionLabel.isHidden = model.description == nil
+        
+        feedImageView.image = UIImage(named: model.imageName)
+    }
 }
