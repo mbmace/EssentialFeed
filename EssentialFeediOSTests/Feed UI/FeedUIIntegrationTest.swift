d9de1a3ce8fd887f4de3f2ddf942c70f3b26bfe3
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-07-08 11:31:41 +0200

    Hide feed load error on reload

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTest.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTest.swift
@@ -80,7 +80,7 @@ final class FeedUIIntegrationTest: XCTestCase {
         assertThat(sut, isRendering: [image0])
     }
     
-    func test_loadFeedCompletion_rendersErrorMessageOnError() {
+    func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
@@ -88,6 +88,9 @@ final class FeedUIIntegrationTest: XCTestCase {
         
         loader.completeFeedLoadingWithError(at: 0)
         XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(sut.errorMessage, nil)
     }
     
     func test_feedImageView_loadsImageURLWhenVisible() {
