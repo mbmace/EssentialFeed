76e2df45aa98b206972dc782a84e4d405bd3618e
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-07-08 14:52:50 +0200

    Move FeedViewModel to separate file

---
+++ EssentialFeed/Feed Presenation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Matthias Sehrbrock on 08.07.21.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
